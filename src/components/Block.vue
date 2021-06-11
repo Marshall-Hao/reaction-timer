c467115dce57879a17dff2615537f2488173fd99
marshall chen <marshallchan666@outlook.com>
2021-06-11 14:30:13 +0800

    implement reaction time with setInterval and clearInterval

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="showBlock" class="block rounded-2xl p-36 text-5xl font-bold text-white bg-indigo-500 text-center">
+    <div v-if="showBlock" @click="stopTimer" class="block rounded-2xl p-36 text-5xl font-bold text-white bg-indigo-500 text-center">
         click me
     </div>
 </template>
@@ -9,13 +9,26 @@ export default {
     props: ['delay'],
     data() {
         return {
-            showBlock:false
+            showBlock:false,
+            timer: null,
+            reactionTime: 0
         }
     },
     mounted() {
         setTimeout(() => {
            this.showBlock = true 
+           this.startTimer()
         }, this.delay)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+        },
     },
 }
 </script>
