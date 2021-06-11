450446d697bcef57c3a7024db55109e23d4436a9
marshall chen <marshallchan666@outlook.com>
2021-06-11 14:19:45 +0800

    understand the basic hook of vue, create block components and mount it with a delay

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
-<h1>Marshall Reaction Timer</h1>
+<div class="flex grid flex-col justify-center">
+  <h1 class="text-indigo-700 text-4xl text-center p-10 font-bold">Marshall Reaction Timer</h1>
+    <button class="font-bold justify-self-center bg-indigo-500 px-12 py-2 rounded-2xl shadow-lg text-white text-center focus:outline-none" 
+  @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"  class="justify-self-center mt-20"/>
+</div>
 </template>
 
 <script>
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
   components: {
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #444;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-if="showBlock" class="block rounded-2xl p-36 text-5xl font-bold text-white bg-indigo-500 text-center">
+        click me
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock:false
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+           this.showBlock = true 
+        }, this.delay)
+    },
+}
+</script>
