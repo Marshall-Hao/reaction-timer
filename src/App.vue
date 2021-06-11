9d4f235ed8475b98bc8f0b9b483777f56ce97927
marshall chen <marshallchan666@outlook.com>
2021-06-11 14:40:36 +0800

    implemet results

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,39 @@
   <h1 class="text-indigo-700 text-4xl text-center p-10 font-bold">Marshall Reaction Timer</h1>
     <button class="font-bold justify-self-center bg-indigo-500 px-12 py-2 rounded-2xl shadow-lg text-white text-center focus:outline-none" 
   @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay"  class="justify-self-center mt-20"/>
+  <Block @end="endGame" v-if="isPlaying" :delay="delay"  class="justify-self-center mt-20"/>
+  <Results class="justify-self-center mt-10 text-2xl font-semibold"  v-if="showResults" :score="score" />
 </div>
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
     Block,
+    Results,
   },
   data() {
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 }
---
+++ src/components/Results.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>Reaction Time: {{ score}}</p>
+</template>
+
+<script>
+export default {
+    props: ['score']
+}
+</script>
